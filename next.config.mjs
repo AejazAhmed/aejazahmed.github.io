9b39a0ae25da64cc4577c1152be63f0c4705aa8f
AEJAZ AHMED DHOTEGHAR <AejazAhmed@users.noreply.github.com>
2025-06-12 04:35:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // Essential for static HTML export
+  reactStrictMode: true, // Recommended for React apps
+  basePath:  '',
+  assetPrefix: '',
+
+  images: {
+    unoptimized: true, // Recommended for static export as Next.js image optimization needs a server
+  },
 };
 
 export default nextConfig;
